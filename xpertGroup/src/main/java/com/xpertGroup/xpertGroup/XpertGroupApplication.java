0bd09c7b943441d0024f98ddb7dca493855a32e5
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2019-07-06 12:38:43 -0500

    inicializar proyecto

---
+++ xpertGroup/src/main/java/com/xpertGroup/xpertGroup/XpertGroupApplication.java
@@ -0,0 +1,13 @@
+package com.xpertGroup.xpertGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpertGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpertGroupApplication.class, args);
+	}
+
+}
